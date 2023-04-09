4be4f1f7d64d399fb65228388bcec3097b2a6117
Luciano <luciano.rodriguez59@gmail.com>
2023-04-09 11:13:08 -0300

    add socket.io

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from 'express';
-
+import { Server } from 'socket.io';
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Express vercel app response');
+const http = app.listen(PORT, () => console.log('Server on port 8080'));
+const io = new Server(http, {
+  cors: {
+    origin: '*',
+  },
 });
 
-app.listen(3000, () => console.log('Server on port 3000'));
+app.get('/', (req, res) => {
+  res.status(200).send('Vercel response');
+});
+
+io.on('connection', socket => {
+  console.log('Nuevo usuario!');
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado!');
+  });
+
+  socket.on('newMessage', data => {
+    console.log(data);
+    io.emit('updateMessages', data);
+  });
+});
